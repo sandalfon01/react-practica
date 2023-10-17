24a7a2dd553bdf001d3ddc2bd46963d2f34c5a79
sandalfon01 <sandalfon01@gmail.com>
2023-10-16 19:08:21 -0500

    usando los eventos en el reaxt (￣y▽￣)╭ Ohohoho.....

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,7 +1,17 @@
+import React from 'react';
 import './TodoSearch.css'
 function TodoSearch(){
+  const [searchValue, setSearchValue] = React.useState('');
+
+  console.log('los usuarios estan buscando todos de ' + searchValue);
+
     return (
-      <input placeholder="Cortar cebolla" className='TodoSearch'/>
+      <input placeholder="Cortar cebolla" className='TodoSearch'
+        value={searchValue}
+        onChange={(event) => {
+          setSearchValue(event.target.value);
+        }}
+      />
     ); 
   }
 
