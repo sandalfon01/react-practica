3c43d32d2365b5c6c1885e02bdfcb693f6745cb7
sandalfon01 <sandalfon01@gmail.com>
2023-10-20 18:07:49 -0500

    ya el aplicativo se siente vivio y se corrigió el daño (～￣▽￣)～(～￣▽￣)～

--- src/App.js
+++ src/App.js
@@ -22,8 +22,22 @@ function App() {
   const totalTodos= todos.length;
 
   const searchedTodos = todos.filter((todo) => {return todo.text.toLowerCase().includes(searchValue.toLowerCase())});
-
-  console.log('los usuarios estan buscando todos de ' + searchValue);
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);  
+  };
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);  
+  };
   return (
     <>
       <TodoCounter completed={completedTodos} total={totalTodos} />
@@ -35,7 +49,8 @@ function App() {
       <TodoList>
         {searchedTodos.map(todo => (
           <TodoItem key={todo.text} text={todo.text} completed={todo.completed}
-          
+          onComplete={() => completeTodo(todo.text)}
+          onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -7,7 +7,9 @@ function TodoItem(props){
         onClick={props.onComplete}
         >V</span>
         <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
-        <span className='Icon Icon-delete'>X</span>
+        <span className='Icon Icon-delete'
+          onClick={props.onDelete}
+        >X</span>
       </li>
     ); 
   }
