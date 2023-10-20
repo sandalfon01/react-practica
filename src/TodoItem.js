65304a69c014fc6519308f015bf9e7f71c7e844c
sandalfon01 <sandalfon01@gmail.com>
2023-10-20 18:49:24 -0500

    dandole los iconos que debe tener con react-icons φ(*￣0￣)φ(*￣0￣)

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,15 +1,17 @@
 import './TodoItem.css'
+import { GiConfirmed } from "react-icons/gi";
+import { GiCancel } from "react-icons/gi";
 function TodoItem(props){
     return (
       <li className='TodoItem'>
         <span className={`Icon Icon-check 
         ${props.completed && 'Icon-check--active'}`}
         onClick={props.onComplete}
-        >V</span>
+        ><GiConfirmed/></span>
         <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
         <span className='Icon Icon-delete'
           onClick={props.onDelete}
-        >X</span>
+        ><GiCancel/></span>
       </li>
     ); 
   }
